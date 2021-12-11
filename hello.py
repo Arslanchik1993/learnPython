2471d25a6ecb36d228c1cec75f60dc4ea0b2e694
Arslan <arslanrozyjumayev1993@gmail.com>
2021-12-11 21:33:07 +0300

    add everything please

--- hello.py
+++ hello.py
@@ -1,7 +1,35 @@
-# All praise belongs to God
+# All Glory belongs to God
+
+#Mastering the for loop
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-print(len('hello from Arslan'))
-print(len('hello from Arslan'))
-print(len('hello from Arslan'))
-print(len('hello from Arslan'))
+#When you write a for loop it's important to differentiate when u write it with "in range" or without it
+
+#simple for loop that prints a string and it's index 10 times (starting with 0)
+# for i in range(10):
+    # print(f"number:{i}")
+
+#When there is no in range the loop gets the actal value
+# for i in alphabet:
+    # print(i)
+
+#Loop that apparently has to changes values in our list into numbers but it doesn't cause TypeError: 'str' object does not support item assignment
+# b = 0  
+# for i in alphabet:
+#     alphabet[i] = str(b)
+
+#but let's try with a new empty string this doesn't wor either
+# b = 0
+# # c = ''
+# for i in alphabet:
+#     c[i] = str(b)
+#     b = b + 1
+#     print(c)
+
+
+# simple program for making a dictionary from your string.
+b = 0
+for i in alphabet:
+    print(f"'{i}':'{b}'", end=",")
+    b = b+1
+        
